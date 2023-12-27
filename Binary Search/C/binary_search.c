aae769fd0e68149bbaa797244ce4c05e2ebff3f7
Rachel Sanford <rachelsa@va-g5.kzn.21-school.ru>
2023-12-27 11:22:17 +0300

    minor fix

--- Binary Search/C/binary_search.c
+++ Binary Search/C/binary_search.c
@@ -12,17 +12,15 @@ int main(int argc, const char *argv[]){
     int *res = (int*)calloc(sizeof(int), 1024);
 
     string_to_int(size, argv, res);
-    printf("%d\n", res[1]);
     qsort(res, size, sizeof(int), cmpfunc);
 //    binary_search();
-    printf("%d\n", res[0]);
+
     return 0;
 }
 
 void string_to_int(int size, const char **argv, int *result){
-  for (int i = 0; i < size && argv[i + 3]; ++i) {
+  for (int i = 0; i < size && argv[i + 3]; ++i)
     result[i] = atoi(argv[i + 3]);
-  }
 }
 
 int cmpfunc (const void * a, const void * b) {
