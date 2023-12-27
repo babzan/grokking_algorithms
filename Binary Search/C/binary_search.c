f17544efe66460eb72f770763076b587264abe41
Rachel Sanford <rachelsa@va-g5.kzn.21-school.ru>
2023-12-27 11:37:04 +0300

    bs almost done. makefile + bs fix

--- Binary Search/C/binary_search.c
+++ Binary Search/C/binary_search.c
@@ -4,7 +4,8 @@
 #include <stdlib.h>
 
 void string_to_int(int size, const char **argv, int *result);
-int cmpfunc (const void * a, const void * b); 
+//int cmpfunc (const void * a, const void * b); 
+int binary_search(int *res, int item, int size);
 
 int main(int argc, const char *argv[]){
     int size = atoi(argv[1]);
@@ -12,10 +13,27 @@ int main(int argc, const char *argv[]){
     int *res = (int*)calloc(sizeof(int), 1024);
 
     string_to_int(size, argv, res);
-    qsort(res, size, sizeof(int), cmpfunc);
-//    binary_search();
-
+    //qsort(res, size, sizeof(int), cmpfunc);
+    int ans = binary_search(res, item, size);
+    printf("%d\n", ans);
     return 0;
+}
+
+int binary_search(int *res, int item, int size){
+  int low = 0;
+  int high = size - 1;
+  int mid = 0;
+  while (low <= high) {
+    mid = (low + high) / 2;
+
+    if (res[mid] == item)
+      return mid;
+    else if (res[mid] > item)
+      high = mid - 1;
+    else if (res[mid] < item)
+      low = mid - 1;
+  }
+  return -1;
 }
 
 void string_to_int(int size, const char **argv, int *result){
@@ -23,9 +41,10 @@ void string_to_int(int size, const char **argv, int *result){
     result[i] = atoi(argv[i + 3]);
 }
 
+/*
 int cmpfunc (const void * a, const void * b) {
    return ( *(int*)a - *(int*)b );
 }
-
+*/
 
 
